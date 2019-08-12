f600f1456b70548216bdb6415011f8d3556735f4
jongsung <42264866+jyholic1209@users.noreply.github.com>
2019-08-12 22:05:05 +0900

    spring boot 1st step
    project 생성 및 bootstrap, jquery 적용

---
+++ src/main/java/com/jy/SpbootExApplication.java
@@ -0,0 +1,13 @@
+package com.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootExApplication.class, args);
+	}
+
+}
